c9219e8bfb2d7c8f4a9bf2dfaddaa5103ba0bbd0
edwardpriyatna <edwardpriyatnax@gmail.com>
2025-04-01 14:40:20 +0800

    how do I improt sklearn scikit-learn or whatever its called

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import joblib
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 model = joblib.load("decision_tree_model.pkl")
 # Sidebar Navigation
